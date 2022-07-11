2f628da43622a1eb572674b2f4506a93d3d663de
afshin sadeghi <sadeghi.afshin@gmail.com>
2022-07-11 13:10:08 +0200

    setting paths

--- getwebpage.py
+++ getwebpage.py
@@ -6,7 +6,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
-
+#in case using ChromeDriverManager enable this
 #from webdriver_manager.chrome import ChromeDriverManager
 import logging
 import os
